f5bc95a9244cfc231c553599bc30f6cbe504b4ff
Erick Ratamero <erickmartins@gmx.net>
2018-08-21 15:57:07 +0100

    changes to add ome-tiffs

--- concat_files_janelia.py
+++ concat_files_janelia.py
@@ -3,6 +3,9 @@ from omero.gateway import BlitzGateway
 from Parse_OMERO_Properties import USERNAME, PASSWORD, HOST, PORT
 # from Parse_OMERO_Properties import imageId, datasetId
 import sys
+import os
+import omero.util.script_utils as script_utils
+from omero.constants.namespaces import NSCREATED, NSOMETIFF
 
 # Create a connection
 # =================================================================
@@ -15,6 +18,32 @@ ret = conn.setGroupForSession(long(sys.argv[1]))
 
 # Retrieve image in specified dataset
 # =================================================================
+
+
+def save_as_ome_tiff(conn, image, folder_name=None):
+    """
+    Saves the image as an ome.tif in the specified folder
+    """
+
+    extension = "ome.tif"
+    name = os.path.basename(image.getName())
+    iid = image.getId()
+    img_name = "%s-%d.%s" % (name, iid, extension)
+    if folder_name is not None:
+        img_name = os.path.join(folder_name, img_name)
+    # check we don't overwrite existing file
+    i = 1
+    path_name = img_name[:-(len(extension) + 1)]
+    while os.path.exists(img_name):
+        img_name = "%s_(%d).%s" % (path_name, i, extension)
+        i += 1
+
+    print("  Saving file as: %s" % img_name)
+    file_size, block_gen = image.exportOmeTiff(bufsize=65536)
+    with open(str(img_name), "wb") as f:
+        for piece in block_gen:
+            f.write(piece)
+
 
 datasetId = sys.argv[2]
 # print(datasetId)
@@ -67,10 +96,14 @@ def planeGen():
         # print(fileId)
         original = conn.getObject("Image", fileId)
         pixels.append(original.getPrimaryPixels())
+    # print(pixels)
     for z in range(sizeZ):          # all Z sections
+        # print(z)
         for c in range(sizeC):
+            # print(c)
             for t in range(sizeT):      # all time-points
                 print("Plane: ", z, c, t)
+                # print(pixels[t].getPlane(0, c, z))
                 yield pixels[t].getPlane(0, c, z)
 
 
@@ -79,3 +112,22 @@ print(sizeZ, sizeC, sizeT)
 new_img = conn.createImageFromNumpySeq(
     planeGen(), "SingleTimelapse", sizeZ, sizeC, sizeT, description=desc,
     dataset=dataset)
+
+save_as_ome_tiff(conn, new_img)
+
+folder_name = ""
+exp_dir = ""
+ometiff_ids = [t.id for t in dataset.listAnnotations(ns=NSOMETIFF)]
+#conn.deleteObjects("Annotation", ometiff_ids)
+extension = "ome.tif"
+name = os.path.basename(new_img.getName())
+iid = new_img.getId()
+img_name = "%s-%d.%s" % (name, iid, extension)
+export_file = img_name
+namespace = NSOMETIFF
+output_display_name = "OME-TIFF"
+mimetype = 'image/tiff'
+
+file_annotation, ann_message = script_utils.create_link_file_annotation(
+    conn, export_file, dataset, output=output_display_name,
+    namespace=namespace, mimetype=mimetype)
